dd230dc4964fd7b7e1e28b1401f6f520578cbbcb
lhy <lianghaoyuan0998@wezhuiyi.com>
2021-11-24 19:51:12 +0800

    v1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='lhy-utils',
-    version='0.0.1',
+    version='0.0.2',
     author='lhy',
     url='https://github.com/lhy2749',
     author_email='379970083@qq.com',
