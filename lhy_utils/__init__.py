32fe7b4f43f82cb67c1ce13bfdc9ba2b04a82934
lhy <lianghaoyuan0998@wezhuiyi.com>
2022-04-28 17:41:01 +0800

    v0.1.14，write_excel, pb2saved_model

--- lhy_utils/__init__.py
+++ lhy_utils/__init__.py
@@ -4,3 +4,4 @@ from .lhy_log_utils import *
 from .lhy_net_utils import *
 from .lhy_tool_utils import *
 from .lhy_module_utils import *
+from .lhy_tf_utils import *
--- lhy_utils/lhy_tool_utils.py
+++ lhy_utils/lhy_tool_utils.py
@@ -7,6 +7,7 @@ import time
 from collections import Counter
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import copy
+import xlsxwriter
 
 import numpy as np
 
@@ -14,9 +15,9 @@ import numpy as np
 def read_gpu(return_least=False):
     import pynvml
     pynvml.nvmlInit()
-    deviceCount = pynvml.nvmlDeviceGetCount()
+    device_count = pynvml.nvmlDeviceGetCount()
     gpu_list = []
-    for i in range(deviceCount):
+    for i in range(device_count):
         handle = pynvml.nvmlDeviceGetHandleByIndex(i)
         meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
         name = pynvml.nvmlDeviceGetName(handle).decode()
@@ -88,15 +89,11 @@ def exec_shell(cmd):
         status: 执行状态
     """
     print(cmd)
-    regex = re.compile(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}")
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
     while p.poll() is None:
         line = p.stdout.readline().strip().decode()
         if line:
-            if re.search(regex, line) is None:
-                print(line)
-            else:
-                print(line)
+            print(line)
     status = p.returncode
     if status != 0:
         print(f'exec cmd failed. {cmd}')
@@ -180,3 +177,113 @@ class MultiThreadBar:
         value = self.data[self.index]
         self.index += 1
         return value
+
+
+def write_excel(path: str,
+                write_data_list: list,
+                sheet_name_list: list = None,
+                format_title: dict = None,
+                format_content: dict = None,
+                row_height: list = None,
+                col_width: list = None):
+    """
+
+    :param path: 文件路径
+    :param write_data_list: 文件内容, 几张表就几个data，data里面的结构是{"表头": [内容1,内容2,...]}
+    :param sheet_name_list:
+    :param format_title:
+    :param format_content:
+    :param row_height:
+    :param col_width:
+    :return:
+    """
+    # 构建A-Z到0-25的映射
+    char2num = {chr(i): i - ord("A") for i in range(ord("A"), ord("Z") + 1)}
+    char2num.update({chr(i): i - ord("a") for i in range(ord("a"), ord("z") + 1)})
+    # 构建format
+    format_t = {"bold": True, "align": "center", "valign": "vcenter", "text_wrap": True}
+    format_c = {"align": "center", "valign": "vcenter", "text_wrap": True}
+    if format_title is not None:
+        format_t.update(format_title)
+    if format_content is not None:
+        format_c.update(format_content)
+    # 构建sheet_name
+    sheet_num = len(write_data_list)
+    if sheet_name_list is None:
+        sheet_name_list = [f"sheet{i}" for i in range(sheet_num)]
+    assert sheet_num == len(sheet_name_list)
+    # 构建数据
+    workbook = xlsxwriter.Workbook(path)
+    format_t = workbook.add_format(format_t)
+    format_c = workbook.add_format(format_c)
+    for data, sheet_name in zip(write_data_list, sheet_name_list):
+        worksheet = workbook.add_worksheet(sheet_name)
+        # 写行高，如果有
+        if row_height is not None:
+            for row, height in row_height:
+                worksheet.set_row(row, height)
+        # 写列宽，如果有
+        if col_width is not None:
+            for col, width in col_width:
+                col = char2num.get(col, col)
+                worksheet.set_column(col, col, width)
+        title_list = list(data.keys())
+        for title_index, title in enumerate(title_list):
+            worksheet.write(0, title_index, title, format_t)
+            for item_index, item in enumerate(data[title]):
+                worksheet.write(item_index + 1, title_index, item, format_c)
+    workbook.close()
+
+
+class ApiTestBase:
+    def __init__(self, url=None, port=None, wait_time=300, batch_size=1, work_num=1,
+                 request_type="g", cpu_num=None, logger=None):
+        self.url = url
+        self.port = port
+        self.wait_time = wait_time  # 压测时间
+        self.batch_size = batch_size
+        self.work_num = work_num  # 并发请求数
+        self.request_type = request_type  # 请求类型，g(grpc)或者h(http)
+        self.cpu_num = cpu_num  # 服务端是否限制了核数，用于统计qps/c
+        self.print = print if logger is None else logger.info
+        self.pool = ThreadPoolExecutor(self.work_num)  # 压测线程池
+        self.success_list = list()  # 成功次数
+        self.failure_list = list()  # 失败次数
+
+    def init_client(self):
+        """
+        初始化请求客户端
+        :return:
+        """
+        raise NotImplemented
+
+    def task(self):
+        raise NotImplemented
+
+    def main(self):
+        self.print("task start")
+        task_list = []
+        for _ in range(self.work_num):
+            t = self.pool.submit(self.task)
+            task_list.append(t)
+        self.pool.shutdown()
+        for t in task_list:
+            success_times, failure_times = t.result()
+            self.success_list.append(success_times)
+            self.failure_list.append(failure_times)
+        self.report()
+
+    def report(self):
+        """
+        获得统计结果
+        :return:
+        """
+        total_success_times = sum(self.success_list)
+        total_failure_times = sum(self.failure_list)
+        failure_rate = round(total_failure_times / (total_success_times + total_failure_times), 3)
+        qps = round(total_success_times / self.wait_time, 2)
+        self.print(f"qps: {qps}")
+        self.print(f"failure rate: {failure_rate}")
+        if self.cpu_num is not None:
+            qps_c = round(qps / self.cpu_num, 2)
+            self.print(f"qps/c: {qps_c}")
